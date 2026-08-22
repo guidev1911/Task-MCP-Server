7a4df83e3ef6fcb1a7fac487aae97d06728ec476
guidev1911 <guilhermereal1911@gmail.com>
2026-08-21 21:26:14 -0300

    first commit

---
+++ src/main/java/com/guidev/task_mcp_server/TaskMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.guidev.task_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidev/task_mcp_server/TaskMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidev.task_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
