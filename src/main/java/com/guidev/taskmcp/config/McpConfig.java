a7a5624c1fa12d9d4c3669165cd03b37cc62e4b6
guidev1911 <guilhermereal1911@gmail.com>
2026-08-22 22:09:19 -0300

    exibindo tools no MCP inspector

--- src/main/java/com/guidev/taskmcp/config/McpConfig.java
+++ src/main/java/com/guidev/taskmcp/config/McpConfig.java
@@ -1,7 +1,7 @@
 package com.guidev.taskmcp.config;
 
 import com.guidev.taskmcp.tool.TaskTool;
-import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class McpConfig {
 
     @Bean
-    public ToolCallback[] taskTools(TaskTool taskTool) {
-        return MethodToolCallbackProvider
-                .builder()
+    public ToolCallbackProvider taskTools(TaskTool taskTool) {
+        return MethodToolCallbackProvider.builder()
                 .toolObjects(taskTool)
-                .build()
-                .getToolCallbacks();
+                .build();
     }
 }
